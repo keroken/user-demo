f7c788cb31350ddae676fee4d960b3f9108b2a89
keroken <kenichi.shinagawa@gmail.com>
2021-10-15 23:09:29 +0900

    base project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [userList, setUserList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleFetchUser = () => {
+    setIsLoading(true);
+    setIsError(false);
+
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(result => {
+        const users = result.data.map(user => ({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          city: user.city,
+        }));
+        setUserList(users);
+      })
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>User Demo</h3>
+      <button onClick={handleFetchUser}>GET User</button>
+      {isError && <p style={{ color: 'red' }}>Error!</p>}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        userList.map(user => (
+          <p key={user.id}>{`${user.id}: ${user.name} from ${user.city}`}</p>
+        ))
+      )}
     </div>
   );
-}
+};
 
 export default App;
